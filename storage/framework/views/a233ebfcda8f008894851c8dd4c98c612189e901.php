14eb3300071d5ee42657d585f5a9a35eefced11e
jedlaurence12 <1801100712@student.buksu.edu.ph>
2021-05-25 08:53:47 +0800

    update 404 page

---
+++ storage/framework/views/a233ebfcda8f008894851c8dd4c98c612189e901.php
@@ -0,0 +1,143 @@
+<html>
+<head>
+    <title> Workkit</title>
+    <link rel="shortcut icon" href="/img/Logo.png" type="image/x-icon">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="<?php echo e(asset('img/Logo.ico')); ?>">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <!-- jQuery and JS bundle w/ Popper.js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <!--font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+
+    <link href="/css/navbar.css" rel="stylesheet">
+    <script>
+        const ROOM_ID = "<%= roomId %>"
+    </script>
+    <script defer src="https://unpkg.com/peerjs@1.2.0/dist/peerjs.min.js"></script>
+    <script src="/socket.io/socket.io.js" defer></script>
+    <script src="/js/script.js" defer></script>
+    <style>
+        #video-grid {
+
+            grid-template-columns: repeat(auto-fill, 30vw);
+            grid-auto-rows: 30vh;
+            align-self: center;
+            align-content: center;
+            align-items: center;
+        }
+
+        video {
+            width: 50%;
+            height: 50%;
+            object-fit: cover;
+            align-self: center;
+            align-content: center;
+            align-items: center;
+            border-radius: 15px;
+        }
+    </style>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid" style="padding-left:30px">
+    <a class="navbar-brand" href="#">
+    <svg style="margin-right:10px" class="img-fluid" width="55" height="50" viewBox="0 0 58 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="29.9198" cy="27.9762" r="24.9762" fill="#6C63FF"/>
+<circle cx="29.9199" cy="27.9763" r="23.1617" stroke="white"/>
+<path d="M36.7039 34.3404C36.6007 34.5617 36.5058 34.6973 36.4192 34.7473C36.2286 34.8573 36.0537 34.8543 35.8944 34.7385C34.7748 33.9992 33.5318 32.8463 32.1654 31.2796C32.1081 31.2203 32.0621 31.2007 32.0275 31.2207L31.9316 31.4146C31.9439 32.516 31.8433 33.6017 31.6298 34.6719C31.4336 35.732 31.2299 36.5193 31.0189 37.0338L30.7023 37.8054C30.6191 38.0613 30.5255 38.2193 30.4216 38.2793C30.2311 38.3893 30.0512 38.3777 29.8819 38.2444C29.0809 37.737 27.8242 36.5804 26.1119 34.7746L25.2631 33.8444C24.054 32.5102 23.0112 31.4841 22.1348 30.7661C21.2757 30.0381 20.7146 29.6462 20.4514 29.5903L20.0566 29.5064C19.8707 29.5445 19.7427 29.5029 19.6727 29.3816C19.5327 29.1392 19.7378 28.6743 20.288 27.9872C20.8554 27.2901 21.3383 26.8265 21.7367 26.5965C22.1351 26.3665 22.4899 26.3811 22.8011 26.6402C23.1197 26.8719 23.8425 27.6439 24.9697 28.9562C27.9371 32.416 29.5854 34.0509 29.9145 33.8609C30.0184 33.8009 30.081 33.6493 30.1022 33.4061C30.1308 33.1355 30.1567 32.8204 30.1799 32.4606C30.2204 32.0908 30.2116 31.4954 30.1533 30.6746C30.0951 29.8537 29.926 29.2008 29.646 28.7158C29.596 28.6292 29.3554 28.3524 28.9241 27.8855C28.4829 27.4012 27.996 26.978 27.4635 26.6157C26.9384 26.2261 26.5865 26.0366 26.4079 26.0473C25.9962 26.0541 25.7553 25.9969 25.6853 25.8757C25.5653 25.6678 25.7803 25.2203 26.3305 24.5332C26.8706 23.8287 27.4265 23.3115 27.9981 22.9815C28.2579 22.8315 28.5297 22.8824 28.8137 23.1342C29.5435 23.7982 30.7002 25.0818 32.284 26.9849C34.3116 29.4169 35.5246 30.5179 35.923 30.2879C36.0616 30.2079 36.1594 29.8974 36.2165 29.3563C36.2637 28.798 36.2021 28.0714 36.0319 27.1766C35.8791 26.2718 35.6526 25.5596 35.3526 25.04C35.0426 24.5031 34.7817 24.1111 34.5697 23.8639C34.3651 23.5895 34.0228 23.3367 33.543 23.1057C33.0632 22.8746 32.6438 22.8281 32.2847 22.9662C31.9156 23.0869 31.686 23.0693 31.596 22.9134C31.456 22.6709 31.6311 22.1542 32.1213 21.3631C32.6287 20.5621 33.2635 19.9415 34.0256 19.5015C34.7531 19.0815 35.5368 19.599 36.3768 21.0539C37.4268 22.8726 37.9699 25.0532 38.006 27.5958C38.0421 30.1383 37.6081 32.3866 36.7039 34.3404Z" fill="white"/>
+</svg><span class="f-12">Workkit</span></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse pl-5" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+      </ul>
+      <span class="navbar-text pl-5">
+        <ul class="navbar-nav pl-5">
+        <li class="nav-item dropdown pl-5">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <?php echo e($LoggedUserInfo['fullname']); ?>
+
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="<?php echo e(route('logout')); ?>">Sign out</a></li>
+          </ul>
+      </span>
+    </div>
+  </div>
+</nav>
+
+    <div class="container-customer" style="position:relative">
+       <center><div id="video-grid"></div><br></center>
+        <div class="chat-row p-0">
+            <div class="chat-content p-0">
+                <ul>
+
+                </ul>
+            </div>
+
+            <div class="chat-section" style="position:fixed;bottom:0;left:5%;width:90%">
+                <div class="chat-box">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">Say something</span>
+                        <div class="chat-input form-control" id="chatInput" contenteditable=""></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+        <script src="https://cdn.socket.io/4.0.1/socket.io.min.js" integrity="sha384-LzhRnpGmQP+lOvWruF/lgkcqD+WDVt9fU3H4BWmwP5u5LTmkUGafMcpZKNObVMLU" crossorigin="anonymous"></script>
+
+
+        <script>
+            $(function() {
+                let ip_address = '127.0.0.1';
+                let socket_port = '3000';
+                let socket = io(ip_address + ':' + socket_port);
+
+                let chatInput = $('#chatInput');
+                chatInput.keypress(function(e) {
+                    let message = $(this).html();
+                    var name = '<?php echo e($LoggedUserInfo['fullname']); ?>';
+                    console.log(message);
+                    var total = name + ' : ' + message;
+                    var total2 = message + ' : ' + name ;
+                    if(e.which === 13 && !e.shiftKey) {
+                        socket.emit('sendChatToServer', total);
+                        display(total2);
+                        chatInput.html('');
+                        return false;
+                    }
+                });
+                const display = function(message) {
+                    $('.chat-content ul').append(`<h5 style="color: white; background: #6C63FF; text-align: end"> ${message}</h5>`);
+                }
+
+                socket.on('sendChatToClient', (message) => {
+                    $('.chat-content ul').append(`<h5> ${message}</h5>`);
+                });
+            });
+        </script>
+
+
+
+<script src="/js/tdash.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+</body>
+</html>
+<?php /**PATH C:\Users\Citycom\Desktop\New folder\saas\resources\views/dashboard.blade.php ENDPATH**/ ?>
